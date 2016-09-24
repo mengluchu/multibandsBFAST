4aa6e83910b45d9eca4e36bb3da52718b79a19f5
Meng Lu <lumeng0312@gmail.com>
2016-09-24 15:55:28 +0200

    wrapvi

--- multibandsBFAST/R/wrap.R
+++ multibandsBFAST/R/wrap.R
@@ -1,5 +1,6 @@
 #' @title  wrap for running different dataset
 #' @param multibandsarrno the second dataset
+#' @import plyr
 #' @export 
 wrap<-function(multibandsarr ,multibandsarrno ,timearr ,history ,
                lastordetect, hisweight, pcacomp, moy, scoreselect, sca )
@@ -17,20 +18,6 @@ wrap<-function(multibandsarr ,multibandsarrno ,timearr ,history ,
              moy=moy, 
              scoreselect=scoreselect,
              sca=sca)
-  
-  res<-c(a1,a2)
-  return(res)
-}
-
-wrapVI<-function(multibandsarr ,multibandsarrno ,timearr ,history ,
-                 pcacomp, moy )
-{
-  a1<-bfmVI(multibandsarr=multibandsarr,timearr=timearr,history=history,
-            moy=moy, 
-  )
-  
-  a2<-bfmVI(multibandsarr=multibandsarrno,timearr=timearr,history=history,
-            moy=moy)
   
   res<-c(a1,a2)
   return(res)
---
+++ multibandsBFAST/R/wrapvi.R
@@ -0,0 +1,15 @@
+#' @title  wrap for univariant
+#' @export
+wrapVI<-function(multibandsarr ,multibandsarrno ,timearr ,history ,
+                 pcacomp, moy )
+{
+  a1<-bfmVI(multibandsarr=multibandsarr,timearr=timearr,history=history,
+            moy=moy, 
+  )
+  
+  a2<-bfmVI(multibandsarr=multibandsarrno,timearr=timearr,history=history,
+            moy=moy)
+  
+  res<-c(a1,a2)
+  return(res)
+}
